82e64bce3671089bf24b032a78bac5969d383198
Dennis Somerville <dennis.somerville@gmail.com>
2014-06-12 22:27:30 -0400

    Added sanity checks for conversion table found online.

--- Katas.RomanNumerals/Tests/CalculatorTests.cs
+++ Katas.RomanNumerals/Tests/CalculatorTests.cs
@@ -11,19 +11,50 @@ namespace Katas.RomanNumerals.Tests
         [TestCase(4, "IV", Description = "A 1 can be placed before a 5 to create a 4", Category = "Special Case")]
         [TestCase(5, "V", Description = "Simple 5 to V")]
         [TestCase(6, "VI", Description = "Adding two roman characters")]
+        [TestCase(7, "VII")]
+        [TestCase(8, "VIII")]
         [TestCase(9, "IX", Description = "A 1 can be placed before a 10 to create a 9", Category = "Special Case")]
         [TestCase(10, "X", Description = "Simple 10 to X")]
+        [TestCase(11, "XI")]
+        [TestCase(14, "XIV")]
 
         public void Convert_Decimal_ToRomanNumber(int number, string expected)
         {
             Assert.That(Calculator.Convert(number), Is.EqualTo(expected));
         }
 
-        [Test]
-        [TestCase(4, "IIII", Description = "Should not have characters 4 times in a row")]
-        public void Convert_ExceptionCases_ShouldNotEqualResult(int number, string expected)
+        [TestCase(1, "I")]
+        [TestCase(2, "II")]
+        [TestCase(3, "III")]
+        [TestCase(4, "IV")]
+        [TestCase(5, "V")]
+        [TestCase(6, "VI")]
+        [TestCase(7, "VII")]
+        [TestCase(8, "VIII")]
+        [TestCase(9, "IX")]
+        [TestCase(10, "X")]
+        [TestCase(20, "XX")]
+        [TestCase(30, "XXX")]
+        [TestCase(40, "XL")]
+        [TestCase(50, "L")]
+        [TestCase(60, "LX")]
+        [TestCase(70, "LXX")]
+        [TestCase(80, "LXXX")]
+        [TestCase(90, "XC")]
+        [TestCase(100, "C")]
+        [TestCase(200, "CC")]
+        [TestCase(300, "CCC")]
+        [TestCase(400, "CD")]
+        [TestCase(500, "D")]
+        [TestCase(600, "DC")]
+        [TestCase(700, "DCC")]
+        [TestCase(800, "DCCC")]
+        [TestCase(900, "CM")]
+        [TestCase(1000, "M")]
+
+        public void Convert_SanityCheck(int number, string expected)
         {
-            Assert.That(Calculator.Convert(number),  !Is.EqualTo(expected));
+            Convert_Decimal_ToRomanNumber(number, expected);
         }
     }
 }
