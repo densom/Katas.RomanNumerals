e0bb8229828aae9f36b536b8d1bf0521178b5ab8
Dennis Somerville <dennis.somerville@gmail.com>
2014-06-12 22:32:09 -0400

    All sanity tests pass.

--- Katas.RomanNumerals/Calculator.cs
+++ Katas.RomanNumerals/Calculator.cs
@@ -15,6 +15,14 @@ namespace Katas.RomanNumerals
                 {5,"V"},
                 {9,"IX"},
                 {10,"X"},
+                {40, "XL"},
+                {50,"L"},
+                {90, "XC"},
+                {100, "C"},
+                {400, "CD"},
+                {500, "D"},
+                {900, "CM"},
+                {1000, "M"},
             };
 
         public static string Convert(int number)
