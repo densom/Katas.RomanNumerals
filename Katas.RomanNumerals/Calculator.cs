86a7a04d3884d47a3de85332a90bc7aa5cd130a0
Dennis Somerville <dennis.somerville@gmail.com>
2014-06-12 22:13:50 -0400

    Adding 2 roman characters - 6 as test case.
    Refactor Convert method to use new algorithm.

--- Katas.RomanNumerals/Calculator.cs
+++ Katas.RomanNumerals/Calculator.cs
@@ -19,27 +19,18 @@ namespace Katas.RomanNumerals
 
         public static string Convert(int number)
         {
-            var resultBuilder = new StringBuilder();
-            var romanNumeral = "I";
-
-            if (number.IsBetween(1, 3))
-            {
-                romanNumeral = "I";
-                int repeatCharacter = number > 3 ? 3 : number;
-
-
-                for (int i = 0; i < repeatCharacter; i++)
-                {
-                    resultBuilder.Append(romanNumeral);
-                }
-            }
-
             if (IsSingleRomanNumeralOrSpecialCase(number))
             {
                 return Numerals[number];
             }
 
-            return resultBuilder.ToString();
+            var firstPart = GetClosestNumeralWithoutGoingOver(number);
+            return firstPart.Value + Convert(number - firstPart.Key);
+        }
+
+        private static KeyValuePair<int, string> GetClosestNumeralWithoutGoingOver(int number)
+        {
+            return Numerals.LastOrDefault(n => n.Key < number);
         }
 
         private static bool IsSingleRomanNumeralOrSpecialCase(int number)
--- Katas.RomanNumerals/Tests/CalculatorTests.cs
+++ Katas.RomanNumerals/Tests/CalculatorTests.cs
@@ -10,6 +10,7 @@ namespace Katas.RomanNumerals.Tests
         [TestCase(3, "III", Description = "Adding a third I")]
         [TestCase(4, "IV", Description = "A 1 can be placed before a 5 to create a 4", Category = "Special Case")]
         [TestCase(5, "V", Description = "Simple 5 to V")]
+        [TestCase(6, "VI", Description = "Adding two roman characters")]
         [TestCase(9, "IX", Description = "A 1 can be placed before a 10 to create a 9", Category = "Special Case")]
         [TestCase(10, "X", Description = "Simple 10 to X")]
 
