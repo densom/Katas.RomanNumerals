964eba23c9a67d7c84528c5c933a177eecbbd805
Dennis Somerville <dennis.somerville@gmail.com>
2014-06-12 21:42:45 -0400

    Introduced 5 to V.

--- Katas.RomanNumerals/Calculator.cs
+++ Katas.RomanNumerals/Calculator.cs
@@ -11,12 +11,36 @@ namespace Katas.RomanNumerals
         public static string Convert(int number)
         {
             var resultBuilder = new StringBuilder();
-            for (int i = 0; i < number; i++)
+            var romanNumeral = "I";
+
+            if (number.IsBetween(1,3))
             {
-                resultBuilder.Append("I");
+                romanNumeral = "I";
+                int repeatCharacter = number > 3 ? 3 : number;
+
+
+                for (int i = 0; i < repeatCharacter; i++)
+                {
+                    resultBuilder.Append(romanNumeral);
+                }
             }
 
+            if (number == 5)
+            {
+                return "V";
+            }
+            
+            
+
             return resultBuilder.ToString();
+        }
+    }
+
+    public static class IntegerExtensions
+    {
+        public static bool IsBetween(this int compareTo, int leftSide, int rightSide)
+        {
+            return (compareTo >= leftSide && compareTo <= rightSide);
         }
     }
 }
--- Katas.RomanNumerals/Tests/CalculatorTests.cs
+++ Katas.RomanNumerals/Tests/CalculatorTests.cs
@@ -8,9 +8,18 @@ namespace Katas.RomanNumerals.Tests
         [TestCase(1, "I", Description = "Simple 1 to I")]
         [TestCase(2, "II", Description = "Adding a second I")]
         [TestCase(3, "III", Description = "Adding a third I")]
+        [TestCase(5, "V", Description = "Simple 5 to V")]
+//        [TestCase(4, "IV", Description = "A one can be placed before a 5 to create a 4", Category = "Special Case")]
         public void Convert_Decimal_ToRomanNumber(int number, string expected)
         {
             Assert.That(Calculator.Convert(number), Is.EqualTo(expected));
-        } 
+        }
+
+        [Test]
+        [TestCase(4, "IIII", Description = "Should not have characters 4 times in a row")]
+        public void Convert_ExceptionCases_ShouldNotEqualResult(int number, string expected)
+        {
+            Assert.That(Calculator.Convert(number),  !Is.EqualTo(expected));
+        }
     }
 }
